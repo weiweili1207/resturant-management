bdffd26d3f01f9f5fbf0110446e33f9a6ac09076
wei <lwei6793@gmail.com>
2024-10-27 23:29:36 -0700

    start project build up

---
+++ dine-master-common/src/main/java/com/dine/exception/AddressBookBusinessException.java
@@ -0,0 +1,9 @@
+package com.dine.exception;
+
+public class AddressBookBusinessException extends BaseException {
+
+    public AddressBookBusinessException(String msg) {
+        super(msg);
+    }
+
+}
---
+++ dine-master-common/src/main/java/com/dine/exception/OrderBusinessException.java
@@ -0,0 +1,9 @@
+package com.dine.exception;
+
+public class OrderBusinessException extends BaseException {
+
+    public OrderBusinessException(String msg) {
+        super(msg);
+    }
+
+}
