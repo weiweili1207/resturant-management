9b7b15d59da96e30a81b958f0378fe2cbd327611
wei <lwei6793@gmail.com>
2024-11-09 00:10:46 -0800

    employee pagination

--- dine-master-common/src/main/java/com/dine/json/JacksonObjectMapper.java
+++ dine-master-common/src/main/java/com/dine/json/JacksonObjectMapper.java
@@ -18,9 +18,9 @@ import java.time.format.DateTimeFormatter;
 import static com.fasterxml.jackson.databind.DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES;
 
 /**
- * 对象映射器:基于jackson将Java对象转为json，或者将json转为Java对象
- * 将JSON解析为Java对象的过程称为 [从JSON反序列化Java对象]
- * 从Java对象生成JSON的过程称为 [序列化Java对象到JSON]
+ * Object Mapper: Converts Java objects to JSON or JSON to Java objects based on Jackson.
+ * The process of parsing JSON into Java objects is called [deserializing Java objects from JSON].
+ * The process of generating JSON from Java objects is called [serializing Java objects to JSON].
  */
 public class JacksonObjectMapper extends ObjectMapper {
 
@@ -31,10 +31,10 @@ public class JacksonObjectMapper extends ObjectMapper {
 
     public JacksonObjectMapper() {
         super();
-        //收到未知属性时不报异常
+        // Do not throw an exception for unknown properties
         this.configure(FAIL_ON_UNKNOWN_PROPERTIES, false);
 
-        //反序列化时，属性不存在的兼容处理
+        // Compatibility handling for non-existent properties during deserialization
         this.getDeserializationConfig().withoutFeatures(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
 
         SimpleModule simpleModule = new SimpleModule()
@@ -45,7 +45,7 @@ public class JacksonObjectMapper extends ObjectMapper {
                 .addSerializer(LocalDate.class, new LocalDateSerializer(DateTimeFormatter.ofPattern(DEFAULT_DATE_FORMAT)))
                 .addSerializer(LocalTime.class, new LocalTimeSerializer(DateTimeFormatter.ofPattern(DEFAULT_TIME_FORMAT)));
 
-        //注册功能模块 例如，可以添加自定义序列化器和反序列化器
+        // Register module functionalities, such as adding custom serializers and deserializers
         this.registerModule(simpleModule);
     }
 }
--- dine-master-common/src/main/java/com/dine/result/PageResult.java
+++ dine-master-common/src/main/java/com/dine/result/PageResult.java
@@ -8,15 +8,15 @@ import java.io.Serializable;
 import java.util.List;
 
 /**
- * 封装分页查询结果
+ * Encapsulates the result of a paginated query
  */
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class PageResult implements Serializable {
 
-    private long total; //总记录数
+    private long total; // Total record count
 
-    private List records; //当前页数据集合
+    private List records; //Data collection for the current page
 
 }
